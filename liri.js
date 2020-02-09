4d7f272e917806e3000bc02df94024b4ec24dc06
Rachel Sipes <rasipes@gmail.com>
2020-02-09 18:23:38 -0500

    added psuedocode to rest of file

--- liri.js
+++ liri.js
@@ -29,8 +29,7 @@ for (var i = 3; i < argv.length; i++) {
     }
 }
 
-//this decides which funciton to run based off of which command is typed.
-
+//this decides which funciton to run based off of which command is typed
 function menu() {
     switch (liriCommand) {
         case "concert-this":
@@ -51,8 +50,7 @@ function menu() {
     }
 }
 
-
-// function that runs if 'concert-this' is typed in.
+// function that runs if 'concert-this' is typed in
 function concertThis() {
     //set axios call url
     var concertURL = "https://rest.bandsintown.com/artists/" + userRequest + "/events?app_id=codingbootcamp";
@@ -75,62 +73,73 @@ function concertThis() {
 }
 
 // spotify
-
 function spotifyThis() {
     spotify.search({
         type: 'track',
         query: userRequest,
-
     },
         function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
-
+            //separates the response array
             for (var i = 0; i < data.tracks.items.length; i++) {
-                // console.log("Spotify: ", data.tracks.items[i])
+                //log artist name
                 console.log("Artist: ", data.tracks.items[i].artists[0].name);
+                //log song name
                 console.log("Song Name: ", data.tracks.items[i].name);
+                //log url
                 console.log("URL: ", data.tracks.items[i].preview_url);
+                //log album name
                 console.log("Album: ", data.tracks.items[i].album.name);
-                console.log("-------------------------------------\n");
-
-
+                console.log("\n-------------------------------------\n");
             }
         })
 }
-
+// ombd
 function movieThis() {
+    //url to search
     var queryURL = "http://www.omdbapi.com/?t=" + userRequest + "&y=&plot=short&apikey=trilogy";
-
+    //axios call
     axios.get(queryURL).then(
         function (response) {
-            console.log("-------------------------------------\n");
+            //log movie title
             console.log("Title: ", response.data.Title);
+            //log release year
             console.log("Release Year: ", response.data.Year);
+            //log imbd rating
             console.log("IMBD Rating: ", response.data.imdbRating);
+            //log rotten tomatoes rating
             console.log("Rotten Tomatoes Rating: ", response.data.Ratings[1].Value);
+            //log country
             console.log("Country: ", response.data.Country);
+            //log language
             console.log("Language: ", response.data.Language);
+            //log plot
             console.log("Plot: ", response.data.Plot);
+            //log actors
             console.log("Actors: ", response.data.Actors);
             console.log("\n-------------------------------------\n");
         }
     )
 }
 
+//reads the random.txt file
 function doThis() {
     fs.readFile("./random.txt", "utf8", function (error, data) {
         if (error) {
             console.log(error);
         }
-
+        //turn string into an array at the ,
         var readMeInput = data.split(",");
-        console.log(data);
+        // console.log(data);
+        //change liricommand definition to index 0
         liriCommand = readMeInput[0];
+        //change userrequest definition to index 1. replace "" with nothing
         userRequest= readMeInput[1].replace(/"/g, '');
+        //call the switch function
         menu();
     })
 }
-
+//invoke switch function
 menu();
