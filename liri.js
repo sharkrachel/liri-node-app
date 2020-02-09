91a105a9d365ce25119ac0521f1cf6c619aead24
Rachel Sipes <rasipes@gmail.com>
2020-02-09 10:12:29 -0500

    installed axios, set variables for user commands

--- liri.js
+++ liri.js
@@ -3,3 +3,21 @@ require("dotenv").config();
 var keys = require("./keys");
 
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios")
+
+// set index 2 definition
+
+var argv = process.argv;
+var liriCommand = process.argv[2];
+var userRequest = " ";
+
+for (var i = 3; i < argv.length; i++) {
+    if (i > 3) {
+        formattedUserRequest = userRequest + " " + argv[i];
+    }
+
+    else {
+        formattedUserRequest += argv[i];
+    }
+}
+
