7ef3a271a29a6e5fa8f0148c2a9962000d4253ef
Rachel Sipes <rasipes@gmail.com>
2020-02-09 12:16:32 -0500

    added in psuedocode for concert-this

--- liri.js
+++ liri.js
@@ -13,6 +13,7 @@ var argv = process.argv;
 var liriCommand = process.argv[2];
 var userRequest = "";
 
+//this handles if the user request is longer than 1 word
 for (var i = 3; i < argv.length; i++) {
     if (i > 3) {
         userRequest = userRequest + "" + argv[i];
@@ -23,23 +24,30 @@ for (var i = 3; i < argv.length; i++) {
     }
 }
 
+//this decides which funciton to run based off of which command is typed.
 switch (liriCommand) {
     case "concert-this":
         concertThis();
         break;
 }
 
-//bands in town
 
+// function that runs if 'concert-this' is typed in.
 function concertThis() {
+    //set axios call url
     var concertURL = "https://rest.bandsintown.com/artists/" + userRequest + "/events?app_id=codingbootcamp";
-
+    //run axios call
     axios.get(concertURL).then(
         function (response) {
+            //loop through results to display all upcoming concert information
             for (var i = 0; i < response.data.length; i++) {
+                //set variable so don't have to type response.data[i] over and over.
                 concertInfo = response.data[i];
+                //console log venue name
                 console.log("Venue: " + concertInfo.venue.name);
+                //log city
                 console.log("Location: " + concertInfo.venue.city);
+                //log concert date
                 console.log("Date: " + moment(concertInfo.datetime).format("MM/DD/YYYY"));
             }
         });
